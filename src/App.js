ea5ed43a398b17f895308332862d6b6307693d62
sinamong0620 <sinaskim60@gmail.com>
2023-09-13 01:25:11 +0900

    DOCS : Error fixing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -6,24 +6,23 @@ import useHttp from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const transformTasks = useCallback((tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-    setTasks(loadedTasks);
-  }, []);
-
-  const httpData = useHttp(transformTasks);
-
-  const { isLoading, error, sendRequest: fetchTasks } = httpData;
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks({
-      url: "https://customhook-http-e6b9f-default-rtdb.firebaseio.com/tasks.json",
-    });
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks(
+      {
+        url: "https://customhook-http-e6b9f-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
   }, [fetchTasks]);
   //무한 루프 발생 함 그렇기 떄문에 이 함수를 다시 재생성되지 않도록 useCallback 으로 감싸야한다.
 
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,34 +1,31 @@
 import react, { useState, useCallback } from "react";
 
-const useHttp = (applyData) => {
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = useCallback(
-    async (requestConfig) => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(requestConfig.url, {
-          method: requestConfig.method ? requestConfig.method : "GET",
-          headers: requestConfig.headers ? requestConfig.headers : {},
-          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
-          //get요청으로 보내면 body는 알아서 무시된대 근데 어차피 더미 데이터를 보낼 필요 없으니깐 null로 보내는거임
-        });
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        //get요청으로 보내면 body는 알아서 무시된대 근데 어차피 더미 데이터를 보낼 필요 없으니깐 null로 보내는거임
+      });
 
-        if (!response.ok) {
-          throw new Error("Request failed!");
-        }
-
-        const data = await response.json();
-        applyData(data);
-      } catch (err) {
-        setError(err.message || "Something went wrong!");
+      if (!response.ok) {
+        throw new Error("Request failed!");
       }
-      setIsLoading(false);
-    },
-    [applyData]
-  );
+
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  }, []);
 
   return {
     isLoading,
